c5e183362f5d5f7ed67289d1d6d1a66c92a874bc
Andres Chapeton <andres.chapeton0206@gmail.com>
2020-05-11 00:24:39 -0600

    Some JS for dark mode

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const checkbox = document.querySelector('#checkbox');
+
+if(window.matchMedia('(prefers-color-scheme: dark)').matches == true){
+    console.log(true);
+    checkbox.setAttribute('checked', true)
+}
+
+checkbox.addEventListener('change', function(event){
+    document.body.classList.toggle('is-dark-mode');
+})
