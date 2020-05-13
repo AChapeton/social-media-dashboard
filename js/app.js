01894a8b30daad86b2b7b5a76084136e46baffa5
Andres Chapeton <andres.chapeton0206@gmail.com>
2020-05-12 20:31:02 -0600

    Logic to change between dark & light mode

--- js/app.js
+++ js/app.js
@@ -5,19 +5,15 @@ if(window.matchMedia('(prefers-color-scheme: dark)').matches == true){
     checkbox.setAttribute('checked', true)
 }
 
+// document.body.style.setProperty('--cardGray', 'blue')
+
 checkbox.addEventListener('change', function(event){
-    document.body.classList.toggle('is-dark-mode');
     if(this.checked){
-        // --cardGray: #252b43;
-        // --baseBackground: #1d2029;
-        // --baseColor: #fff;
-        // --gray: #8088ad;
-        // --lightGray: #1d2029;
-        // --cardTitle: #8088ad;
-        // --switchSpinner: #282943;
-        // --switch: linear-gradient(to right, #4796d2 0%, #37ce8f 100%);
+        document.body.classList.remove('is-light-mode');
+        document.body.classList.add('is-dark-mode');
     }
     else{
-
+        document.body.classList.remove('is-dark-mode');
+        document.body.classList.add('is-light-mode');
     }
 })
